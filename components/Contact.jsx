d36675996839d966c0eeff2a3c43e171bb21fcdf
scoops82 <rupert.taylor1982@gmail.com>
2022-09-07 21:59:50 +0100

    added footer and and netlify form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,13 +1,43 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+
 export default function Contact() {
-  return (
-    <section id="contact" className="bg-base-200 p-4">
-      <h2 className="mb-5 text-5xl font-bold text-center">Contact</h2>
+  const [submitterName, setSubmitterName] = useState("");
+  const router = useRouter();
+  const confirmationScreenVisible =
+    router.query?.success && router.query.success === "true";
+  const formVisible = !confirmationScreenVisible;
+
+  const ConfirmationMessage = (
+    <React.Fragment>
+      <p>
+        Thank you for submitting this form. I should get back to you within
+        24-48 hours.
+      </p>
+
+      <button
+        className="btn btn-primary"
+        onClick={() => router.replace("/contact", undefined, { shallow: true })}
+      >
+        {" "}
+        Submit Another Response{" "}
+      </button>
+    </React.Fragment>
+  );
+
+  const ContactForm = (
+    <>
+      <p className="mb-2 w-72 text-center m-auto">
+        If you would like to get in touch, please fill in the form below.
+      </p>
       <form
-        name="contact"
+        name="contact-form"
         method="POST"
+        action="contact/?success=true"
         className="flex flex-col flex-wrap gap-2 items-center"
         data-netlify="true"
       >
+        <input type="hidden" name="form-name" value="contact-form" />
         <div className="form-control">
           <label className="input-group w-72">
             <span>Name</span>
@@ -54,6 +84,14 @@ export default function Contact() {
           </button>
         </div>
       </form>
+    </>
+  );
+
+  return (
+    <section id="contact" className="bg-base-200 p-4">
+      <h2 className="mb-5 text-5xl font-bold text-center">Contact</h2>
+
+      {formVisible ? ContactForm : ConfirmationMessage}
     </section>
   );
 }
