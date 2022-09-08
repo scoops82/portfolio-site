572d8498ad55b354fc1082dbe5aeb5a3f0215b35
scoops82 <rupert.taylor1982@gmail.com>
2022-09-08 22:15:26 +0100

    light-dark bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class", '[data-mode="night"]'],
+  darkMode: ["class", '[data-theme="night"]'],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
