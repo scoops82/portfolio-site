2325316b897ed9884e09a59ee01d8c372c3e1af0
scoops82 <rupert.taylor1982@gmail.com>
2022-09-08 21:54:22 +0100

    fixed light-dark toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class", '[data-mode="night"]'],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
